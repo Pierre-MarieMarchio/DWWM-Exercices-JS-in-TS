4f3a0c97099339c76d697ee77ab0e21281b7d640
Pierre-Marie <pierremariemarchio@gmail.com>
2024-04-18 16:36:54 +0200

    ex 1 formResult

--- Exercices/exercice 1/app.js
+++ Exercices/exercice 1/app.js
@@ -1,2 +1,26 @@
 "use strict";
-alert("coucou");
+const age = document.getElementById("age");
+const sex = document.getElementById("sex");
+const btnSubmit = document.getElementById("btnSubmit");
+let formResults = {
+    age: "",
+    sex: "",
+};
+const handleform = () => {
+    btnSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        let ageValue = "";
+        let sexValue = "";
+        if (age) {
+            ageValue = age.value;
+        }
+        if (sex) {
+            sexValue = sex.value;
+        }
+        formResults = {
+            age: ageValue,
+            sex: sexValue
+        };
+        return formResults;
+    });
+};
--- TS/exercice 1/app.ts
+++ TS/exercice 1/app.ts
@@ -1,1 +1,32 @@
-alert("coucou");
+const age = document.getElementById("age") as HTMLInputElement;
+const sex = document.getElementById("sex") as HTMLInputElement;
+const btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
+
+
+let formResults = {
+    age: "",
+    sex: "",
+}
+
+const handleform = () => {
+  btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let ageValue: string = "";
+    let sexValue: string = "";
+
+    if (age) {
+      ageValue = age.value;
+    }
+    if (sex) {
+        sexValue = sex.value;
+    }
+    formResults = { 
+        age: ageValue, 
+        sex: sexValue 
+    };
+    return formResults;
+
+  });
+};
+
